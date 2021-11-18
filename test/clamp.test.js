b69c7519a9e961afbdfca962b5807994aa4aa9f9
Matias Laakso <matias.laakso@tuni.fi>
2021-11-18 13:24:01 +0200

    Additional tests for clamp

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -3,28 +3,36 @@ import clamp from "../src/clamp.js";
 var expect = _expect
 
 describe("Clamp", () => {
-    it("Increases number below range to lower limit", () => {
+    it("Increases number below range to lower limit (positive numbers)", () => {
         var result = clamp(1,5,10);
         expect(result).to.equal(5);
     });
-    it("Decreases number above range to upper limit", () => {
+    it("Decreases number above range to upper limit (positive numbers)", () => {
         var result = clamp(12,5,10);
         expect(result).to.equal(10);
     });
-    it("Doesn't change number already in range", () => {
+    it("Doesn't change number already in range (positive numbers)", () => {
         var result = clamp(7,5,10);
         expect(result).to.equal(7);
     });
-    it("Works with negative numbers", () => {
+    it("Doesn't change number already in range (negative numbers)", () => {
         var result = clamp(-7,-5,-10);
         expect(result).to.equal(-7);
     });
-    it("Works when number is already at the lower limit of the range", () => {
+    it("Works when number is already at the lower limit of the range (negative numbers)", () => {
         var result = clamp(-12.2,-5,-12.2);
         expect(result).to.equal(-12.2);
     });
-    it("Works when number is already at the upper limit of the range", () => {
+    it("Works when number is already at the upper limit of the range (negative numbers)", () => {
         var result = clamp(-5,-5,-12.2);
         expect(result).to.equal(-5);
+    });
+    it("Works when number is already at the lower limit of the range (positive numbers)", () => {
+        var result = clamp(0.001, 0.001, 0.1);
+        expect(result).to.equal(0.001);
+    });
+    it("Works when number is already at the upper limit of the range (positive numbers)", () => {
+        var result = clamp(3.12, 0.0, 3.12);
+        expect(result).to.equal(3.12);
     });
 });
