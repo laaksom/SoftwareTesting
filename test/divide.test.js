281cac5a80d3d7eaf32c6ba7575adea44b8c97c0
Katri Leinonen <katri.leinonen@tuni.fi>
2021-11-25 09:47:25 +0200

    Added some basic tests for filter.js, map.js and reduce.js.

--- test/divide.test.js
+++ test/divide.test.js
@@ -6,8 +6,11 @@ describe("Divide", () => {
     it("Basic divide with positive numbers", () => {
         expect(divide(4,2)).to.equal(2)
     });
-    it("Divide with positive numbers to check the remainder", () => {
-        expect(divide(5,2)).to.equal(2,5)
+    it("Divide positive number with negative number", () => {
+        expect(divide(5,-2)).to.equal(-2,5)
+    });
+    it("Dividing negative number with positive number", () => {
+        expect(divide(-1,2)).to.equal(-0,5)
     });
     it("Dividing two negative numbers", () => {
         expect(divide(-1,-2)).to.equal(0,5)
